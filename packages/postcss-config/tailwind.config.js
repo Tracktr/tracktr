47395c066f6ccbd779b503d9694c6270cbebcff9
Patrick Roelofs <contact@patrickroelofs.com>
2024-06-30 17:57:34 +0200

    remove old storybook, rename package.json to @tracktr, add tailwind config

---
+++ packages/postcss-config/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+};
---
+++ packages/ui/postcss.config.js
@@ -0,0 +1,1 @@
+module.exports = require("@tracktr/postcss-config/postcss");
