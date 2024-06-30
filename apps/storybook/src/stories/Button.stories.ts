91b60f6cb29f2222b85f3e9e3058ebf1f3aba172
Patrick Roelofs <contact@patrickroelofs.com>
2024-06-30 14:14:00 +0200

    feat: init turbo project with storybook

---
+++ apps/storybook/src/stories/Button.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "@repo/ui/button";
+
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    appName: "Button",
+    children: "I am a primary button.",
+  },
+};
