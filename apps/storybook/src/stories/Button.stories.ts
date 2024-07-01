0728718a8aab24d640987806086d8a9e233e31d0
Patrick Roelofs <contact@patrickroelofs.com>
2024-07-01 20:28:45 +0200

    Setup unit testing library Fixes #11

--- apps/storybook/src/stories/Button.stories.ts
+++ apps/storybook/src/stories/Button.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "@tracktr/ui/button";
+import { userEvent, within } from "@storybook/test";
 
 const meta = {
   title: "Example/Button",
@@ -13,5 +14,12 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     children: "I am a primary button.",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const button = canvas.getByRole("button");
+
+    await userEvent.click(button);
   },
 };
