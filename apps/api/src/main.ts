e1fc25040deb76d82053ca9bf49ed51841f86f86
Rowan-Paul <rowan-paul@webbio.nl>
2024-07-05 20:01:38 +0200

    feat: Setup Nest with SWC, Fastify, Swagger

---
+++ apps/api/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Tracktr')
+    .setDescription('The Tracktr API description')
+    .setVersion('1.0')
+    .addTag('tracktr')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
