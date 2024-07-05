0d7e1140d91ec0d0228dd9ed64c9300abfabd85d
Patrick Roelofs <contact@patrickroelofs.com>
2024-07-05 15:55:36 +0200

    Add CVA to ui package (#29)
    Fixes #28

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { cva } from "class-variance-authority";
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
   children: ReactNode;
 }
 
-export const Button = ({ children }: ButtonProps) => {
-  return <button className="bg-red-500">{children}</button>;
+const ButtonStyles = cva(["bg-blue-500", "text-white", "px-4", "py-2"]);
+
+export const Button = ({ children }: ButtonProps): JSX.Element => {
+  return <button className={twMerge(ButtonStyles())}>{children}</button>;
 };
